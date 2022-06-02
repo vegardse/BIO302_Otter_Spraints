90bbd9d9a7208015502c56bae8eb7e6f938d009f
vegardse <vse007@uib.com>
2022-06-02 14:16:46 +0200

    Cool

--- Otter Spraints.R
+++ Otter Spraints.R
@@ -1,6 +1,11 @@
 library(usethis)
+library(usethis)
+use_git_config(
+  user.name = "vegardse", 
+  user.email = "vse007@uib.no"
+)
 usethis::use_git()
 git_default_branch_rename()
 git_default_branch_configure(name = "main")
 use_github()
-mmmm
+#Spraints are nice
