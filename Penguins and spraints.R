9fb42b7884868a72dcaf32fc7050740899f2d0a0
vegardse <vse007@uib.com>
2022-06-02 14:51:58 +0200

    Added labs

--- Penguins and spraints.R
+++ Penguins and spraints.R
@@ -6,4 +6,8 @@ library(tidyverse)
 chinstrap <- penguins |> 
   filter(species == "Chinstrap")
 ggplot(data = chinstrap, aes(x = bill_length_mm, y = bill_depth_mm))+
-  geom_point()
+  geom_point()+
+  labs(
+    x = "Bill length (mm)",
+    y = "Bill depth (mm)",
+    title = "Bill length and bill depth" )
